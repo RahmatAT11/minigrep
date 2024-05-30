5c676cd8d020d01864bc3bd15081d4984cdd8b94
ralrafi12@gmail.com <ralrafi12@gmail.com>
2024-05-30 19:45:12 +0700

    Update build function to use iterator

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments")
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query: String = args[1].clone();
-        let file_path: String = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
     
