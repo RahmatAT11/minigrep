b38af0a3f6710a2715293fd7f7a700cd8787204c
ralrafi12@gmail.com <ralrafi12@gmail.com>
2024-05-30 10:41:07 +0700

    Add functionality to search function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::fs;
+use std::{fs, result};
 
 pub struct Config {
     pub query: String,
@@ -26,7 +26,14 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    
+    results
 }
 
 mod tests {
